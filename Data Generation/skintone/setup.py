fdaca2231019d7156d56b2ff12fc16cb10cf590b
Eashaan Katiyar <eashaank@gmail.com>
2021-05-06 17:46:06 -0700

    Fixing submodules 1

--- Data Generation/skintone/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-	name='Skin',
-	version='0.1dev',
-	packages=['skin',],
-	license='Open for Commercial as well as Non-Commercial Use'
-	# long_description=open('README.txt').read()
-)
